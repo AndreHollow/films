14cae536213c9e3e5a0b1ca3195d190000b1b6d6
Andre Hollow <kinomaru@mail.com>
2018-12-17 11:02:12 +0100

    Added folder for gh pages

---
+++ docs/precache-manifest.9534891e7494788f1b62fa97da229c8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b4641cc571ff92284ea",
+    "url": "/films/static/js/runtime~main.4b4641cc.js"
+  },
+  {
+    "revision": "ced0c05f19b67ca519b9",
+    "url": "/films/static/js/main.ced0c05f.chunk.js"
+  },
+  {
+    "revision": "1d4e4995ce7a05485f0a",
+    "url": "/films/static/js/1.1d4e4995.chunk.js"
+  },
+  {
+    "revision": "ced0c05f19b67ca519b9",
+    "url": "/films/static/css/main.ccd14001.chunk.css"
+  },
+  {
+    "revision": "9833dc9854bbe05321172c59e438a3fd",
+    "url": "/films/index.html"
+  }
+];
